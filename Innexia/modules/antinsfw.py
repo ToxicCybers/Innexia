ea42f7ee5add2d156f6fa2f94631759b37e63ef1
Cyber Toxic <84234982+ToxicCybers@users.noreply.github.com>
2022-05-28 11:52:07 +0530

    Update antinsfw.py

--- Innexia/modules/antinsfw.py
+++ Innexia/modules/antinsfw.py
@@ -2,10 +2,10 @@ from os import remove
 
 from pyrogram import filters
 
-from EmikoRobot import pbot, arq, BOT_USERNAME as bn
-from EmikoRobot.utils.errors import capture_err
-from EmikoRobot.utils.permissions import adminsOnly
-from EmikoRobot.ex_plugins.dbfunctions import is_nsfw_on, nsfw_off, nsfw_on
+from Innexia import pbot, arq, BOT_USERNAME as bn
+from Innexia.utils.errors import capture_err
+from Innexia.utils.permissions import adminsOnly
+from Innexia.ex_plugins.dbfunctions import is_nsfw_on, nsfw_off, nsfw_on
 
 __mod_name__ = "Anti-NSFW​"
 
