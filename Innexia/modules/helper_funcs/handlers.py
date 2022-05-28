02935018dfe212bc5cdd318dff3f52359bbbe1f4
Cyber Toxic <84234982+ToxicCybers@users.noreply.github.com>
2022-05-28 19:02:24 +0530

    Update handlers.py

--- Innexia/modules/helper_funcs/handlers.py
+++ Innexia/modules/helper_funcs/handlers.py
@@ -1,6 +1,6 @@
-import EmikoRobot.modules.sql.blacklistusers_sql as sql
-from EmikoRobot import ALLOW_EXCL
-from EmikoRobot import DEV_USERS, DRAGONS, DEMONS, TIGERS, WOLVES
+import Innexia.Database.blacklistusers_sql as sql
+from Innexia import ALLOW_EXCL
+from Innexia import DEV_USERS, DRAGONS, DEMONS, TIGERS, WOLVES
 
 from telegram import Update
 from telegram.ext import CommandHandler, MessageHandler, RegexHandler, Filters
