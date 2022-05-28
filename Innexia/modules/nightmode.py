3cdfc218bb1ec67eeb8e3f6f2fce87b79297abbf
Cyber Toxic <84234982+ToxicCybers@users.noreply.github.com>
2022-05-28 18:08:28 +0530

    Update nightmode.py

--- Innexia/modules/nightmode.py
+++ Innexia/modules/nightmode.py
@@ -1,5 +1,5 @@
 import os
-from EmikoRobot.modules.sql.night_mode_sql import (
+from Innexia.Database.night_mode_sql import (
     add_nightmode,
     rmnightmode,
     get_all_chat_id,
@@ -8,8 +8,8 @@ from EmikoRobot.modules.sql.night_mode_sql import (
 from telethon.tl.types import ChatBannedRights
 from apscheduler.schedulers.asyncio import AsyncIOScheduler 
 from telethon import functions
-from EmikoRobot.events import register
-from EmikoRobot import telethn as tbot, OWNER_ID
+from Innexia.events import register
+from Innexia import telethn as tbot, OWNER_ID
 from telethon import Button, custom, events
 
 hehes = ChatBannedRights(
