7afd0ba876361ead33a7c2f50cc1edf34afac831
Cyber Toxic <84234982+ToxicCybers@users.noreply.github.com>
2022-05-28 09:33:54 +0530

    Update afk_sql.py

--- Innexia/Database/afk_sql.py
+++ Innexia/Database/afk_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from datetime import datetime
 
-from Yone.Database import BASE, SESSION
+from Innexia.Database import BASE, SESSION
 from sqlalchemy import Boolean, Column, BigInteger, UnicodeText, DateTime
 
 
