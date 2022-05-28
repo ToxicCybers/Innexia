3ba27364b9dc96c462fc78048ff1ef8ea836e738
Cyber Toxic <84234982+ToxicCybers@users.noreply.github.com>
2022-05-28 17:59:47 +0530

    Update nsfw.py

--- Innexia/modules/nsfw.py
+++ Innexia/modules/nsfw.py
@@ -4,14 +4,14 @@ import nekos
 import requests
 from PIL import Image
 from telegram import ParseMode
-from EmikoRobot import dispatcher, updater
-import EmikoRobot.modules.sql.nsfw_sql as sql
-from EmikoRobot.modules.log_channel import gloggable
+from Innexia import dispatcher, updater
+import Innexia.Database.nsfw_sql as sql
+from Innexia.modules.log_channel import gloggable
 from telegram import Message, Chat, Update, Bot, MessageEntity
 from telegram.error import BadRequest, RetryAfter, Unauthorized
 from telegram.ext import CommandHandler, run_async, CallbackContext
-from EmikoRobot.modules.helper_funcs.filters import CustomFilters
-from EmikoRobot.modules.helper_funcs.chat_status import user_admin
+from Innexia.modules.helper_funcs.filters import CustomFilters
+from Innexia.modules.helper_funcs.chat_status import user_admin
 from telegram.utils.helpers import mention_html, mention_markdown, escape_markdown
 
 @user_admin
