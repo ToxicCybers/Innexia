fd21aad0c9ae32fb6a8f58aa9fece67b876fab90
Cyber Toxic <84234982+ToxicCybers@users.noreply.github.com>
2022-05-28 18:16:04 +0530

    Update forcesubs.py

--- Innexia/modules/forcesubs.py
+++ Innexia/modules/forcesubs.py
@@ -10,9 +10,9 @@ from pyrogram.errors.exceptions.bad_request_400 import (
 )
 from pyrogram.types import ChatPermissions, InlineKeyboardButton, InlineKeyboardMarkup
 
-from EmikoRobot import DRAGONS as SUDO_USERS
-from EmikoRobot import pbot
-from EmikoRobot.modules.sql import forceSubscribe_sql as sql
+from Innexia import DRAGONS as SUDO_USERS
+from Innexia import pbot
+from Innexia.Database import forceSubscribe_sql as sql
 
 logging.basicConfig(level=logging.INFO)
 
