537f5fa9cfb040f22298f25ef562c29f697eb99a
Cyber Toxic <84234982+ToxicCybers@users.noreply.github.com>
2022-06-08 07:51:25 +0530

    Update __main__.py

--- Innexia/__main__.py
+++ Innexia/__main__.py
@@ -736,8 +736,7 @@ def main():
             updater.bot.set_webhook(url=URL + TOKEN)
 
     else:
-        LOGGER.info("Using long polling.")
-        updater.start_polling(timeout=15, read_latency=4, clean=True)
+        updater.start_polling(allowed_updates=Update.ALL_TYPES, timeout=15, read_latency=4, drop_pending_updates=True)
 
     if len(argv) not in (1, 3, 4):
         telethn.disconnect()
