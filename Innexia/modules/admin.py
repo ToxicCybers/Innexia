c6ec2bb28f5662e00bd883de74e906da9d710c2d
Cyber Toxic <84234982+ToxicCybers@users.noreply.github.com>
2022-06-08 08:49:43 +0530

    Update admin.py

--- Innexia/modules/admin.py
+++ Innexia/modules/admin.py
@@ -158,116 +158,15 @@ def setchat_title(update: Update, context: CallbackContext):
         return
         
         
+
+
+
 @connection_status
 @bot_admin
 @can_promote
 @user_admin
 @loggable
 def promote(update: Update, context: CallbackContext) -> str:
-    bot = context.bot
-    args = context.args
-
-    message = update.effective_message
-    chat = update.effective_chat
-    user = update.effective_user
-    user_id, title = extract_user_and_text(message, args)   
-    promoter = chat.get_member(user.id)
-
-    if (
-        not (promoter.can_promote_members or promoter.status == "creator")
-        and user.id not in DRAGONS
-    ):
-        message.reply_text("You don't have the necessary rights to do that!")
-        return
-
-    user_id = extract_user(message, args)
-
-    if not user_id:
-        message.reply_text(
-            "You don't seem to be referring to a user or the ID specified is incorrect..",
-        )
-        return
-
-    try:
-        user_member = chat.get_member(user_id)
-    except:
-        return
-
-    if user_member.status in ('administrator', 'creator'):
-        message.reply_text("How am I meant to promote someone that's already an admin?")
-        return
-
-    if user_id == bot.id:
-        message.reply_text("I can't promote myself! Get an admin to do it for me.")
-        return
-
-    # set same perms as bot - bot can't assign higher perms than itself!
-    bot_member = chat.get_member(bot.id)
-
-    try:
-        bot.promoteChatMember(
-            chat.id,
-            user_id,
-            can_change_info=bot_member.can_change_info,
-            can_post_messages=bot_member.can_post_messages,
-            can_edit_messages=bot_member.can_edit_messages,
-            can_delete_messages=bot_member.can_delete_messages,
-            can_invite_users=bot_member.can_invite_users,
-            can_manage_voice_chats=bot_member.can_manage_voice_chats,
-            can_restrict_members=bot_member.can_restrict_members,
-            can_pin_messages=bot_member.can_pin_messages,
-        )
-    except BadRequest as err:
-        if err.message == "User_not_mutual_contact":
-            message.reply_text("I can't promote someone who isn't in the group.")
-        else:
-            message.reply_text("An error occured while promoting.")
-        return
-
-    keyboard = InlineKeyboardMarkup([[
-        InlineKeyboardButton(
-            "Demote", callback_data="demote_({})".format(user_member.user.id))
-    ]])
-    if not title:
-        message.reply_text(
-        f"<b>Successfully Promoted</b>\n<b> {mention_html(user_member.user.id, user_member.user.first_name)}</b>\n<b> In Chat {chat.title}</b>\n<b>By {mention_html(user.id, user.first_name)}</b>",        
-        parse_mode=ParseMode.HTML,     
-    )    
-        return
-
-    if len(title) > 16:
-        message.reply_text(
-            "The title length is longer than 16 characters.\nTruncating it to 16 characters.",
-        )
-    try:
-        bot.setChatAdministratorCustomTitle(chat.id, user_id, title)
-    except BadRequest:
-        message.reply_text(
-            "Either they aren't promoted by me or you set a title text that is impossible to set."
-        )
-        return
-
-    message.reply_text(
-        f"<b>Successfully Promoted</b>\n<b> {mention_html(user_member.user.id, user_member.user.first_name)}</b>\n<b> In Chat {chat.title}</b>\n<b>By {mention_html(user.id, user.first_name)}</b>\n<b> with title {title}</b>\n<b>",        
-        parse_mode=ParseMode.HTML,
-    )  
-
-    log_message = (
-        f"<b>{html.escape(chat.title)}:</b>\n"
-        f"#PROMOTED\n"
-        f"<b>Admin:</b> {mention_html(user.id, user.first_name)}\n"
-        f"<b>User:</b> {mention_html(user_member.user.id, user_member.user.first_name)}"
-    )
-
-    return log_message
-
-
-@connection_status
-@bot_admin
-@can_promote
-@user_admin
-@loggable
-def fullpromote(update: Update, context: CallbackContext) -> str:
     bot = context.bot
     args = context.args
 
@@ -317,10 +216,9 @@ def fullpromote(update: Update, context: CallbackContext) -> str:
             can_edit_messages=bot_member.can_edit_messages,
             can_delete_messages=bot_member.can_delete_messages,
             can_invite_users=bot_member.can_invite_users,
-            can_promote_members=bot_member.can_promote_members,
+            can_manage_voice_chats=bot_member.can_manage_voice_chats,
             can_restrict_members=bot_member.can_restrict_members,
             can_pin_messages=bot_member.can_pin_messages,
-            can_manage_voice_chats=bot_member.can_manage_voice_chats,
         )
     except BadRequest as err:
         if err.message == "User_not_mutual_contact":
@@ -335,7 +233,7 @@ def fullpromote(update: Update, context: CallbackContext) -> str:
     ]])
     if not title:
         message.reply_text(
-        f"<b>Successfully Promoted</b>\n<b>{mention_html(user_member.user.id, user_member.user.first_name)}</b>\n<b>In Chat {chat.title}</b>\n<b>By {mention_html(user.id, user.first_name)}</b>\n<b> #FULLPROMOTED </b>",        
+        f"<b>Successfully Promoted</b>\n<b>{mention_html(user_member.user.id, user_member.user.first_name)}</b>\n<b>In Chat {chat.title}</b>\n<b>By {mention_html(user.id, user.first_name)}</b>",        
         parse_mode=ParseMode.HTML,     
     )    
         return
@@ -353,13 +251,13 @@ def fullpromote(update: Update, context: CallbackContext) -> str:
         return
 
     message.reply_text(
-        f"<b>Successfully Promoted</b>\n<b> {mention_html(user_member.user.id, user_member.user.first_name)}</b>\n<b> In Chat {chat.title}</b>\n<b>By {mention_html(user.id, user.first_name)}</b>\n<b> with title {title}</b>\n<b> #FULLPROMOTED</b>",        
+        f"<b>Successfully Promoted</b>\n<b> {mention_html(user_member.user.id, user_member.user.first_name)}</b>\n<b> In Chat {chat.title}</b>\n<b>By {mention_html(user.id, user.first_name)}</b>\n<b> with title {title}</b>",        
         parse_mode=ParseMode.HTML,
     )  
 
     log_message = (
         f"<b>{html.escape(chat.title)}:</b>\n", 
-        f"#FULLPROMOTED\n", 
+        f"#PROMOTED\n", 
         f"<b>Admin:</b> {mention_html(user.id, user.first_name)}\n", 
         f"<b>User:</b> {mention_html(user_member.user.id, user_member.user.first_name)}\n", 
         f"<b>Title:</b> {title}", 
@@ -896,7 +794,7 @@ __help__ = """
 ❂ /fullpromote*:* promotes the user replied to with full rights
 ❂ /demote*:* demotes the user replied to
 ❂ /title <title here>*:* sets a custom title for an admin that the bot promoted
-❂ /admincache*:* force refresh the admins list
+❂ /reload*:* force refresh the admins list
 ❂ /del*:* deletes the message you replied to
 ❂ /purge*:* deletes all messages between this and the replied to message.
 ❂ /purge <integer X>*:* deletes the replied message, and X messages following it if replied to a message.
@@ -926,11 +824,10 @@ PINNED_HANDLER = CommandHandler("pinned", pinned, filters=Filters.chat_type.grou
 INVITE_HANDLER = DisableAbleCommandHandler("invitelink", invite)
 
 PROMOTE_HANDLER = DisableAbleCommandHandler("promote", promote)
-FULLPROMOTE_HANDLER = DisableAbleCommandHandler("fullpromote", fullpromote)
 DEMOTE_HANDLER = DisableAbleCommandHandler("demote", demote)
 
 SET_TITLE_HANDLER = CommandHandler("title", set_title)
-ADMIN_REFRESH_HANDLER = CommandHandler("admincache", refresh_admin, filters=Filters.chat_type.groups)
+ADMIN_REFRESH_HANDLER = CommandHandler("reload", refresh_admin, filters=Filters.chat_type.groups)
 
 dispatcher.add_handler(SET_DESC_HANDLER)
 dispatcher.add_handler(SET_STICKER_HANDLER)
@@ -943,7 +840,6 @@ dispatcher.add_handler(UNPIN_HANDLER)
 dispatcher.add_handler(PINNED_HANDLER)
 dispatcher.add_handler(INVITE_HANDLER)
 dispatcher.add_handler(PROMOTE_HANDLER)
-dispatcher.add_handler(FULLPROMOTE_HANDLER)
 dispatcher.add_handler(DEMOTE_HANDLER)
 dispatcher.add_handler(SET_TITLE_HANDLER)
 dispatcher.add_handler(ADMIN_REFRESH_HANDLER)
@@ -958,10 +854,9 @@ __command_list__ = [
     "adminlist",
     "admins", 
     "invitelink", 
-    "promote", 
-    "fullpromote",    
+    "promote",        
     "demote", 
-    "admincache"
+    "reload"
 ]
 __handlers__ = [
     SET_DESC_HANDLER,
@@ -974,8 +869,7 @@ __handlers__ = [
     UNPIN_HANDLER,
     PINNED_HANDLER,
     INVITE_HANDLER,
-    PROMOTE_HANDLER,
-    FULLPROMOTE_HANDLER,
+    PROMOTE_HANDLER,    
     DEMOTE_HANDLER,
     SET_TITLE_HANDLER,
     ADMIN_REFRESH_HANDLER,
